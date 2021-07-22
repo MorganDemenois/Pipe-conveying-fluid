1be6f8d79f33456e65f3e8c138553c163165361d
MorganDemenois <morgan.demenois@gmail.com>
2021-07-22 14:25:31 -0400

    Update + Mode Notebook

--- Mode_Tracking.py
+++ Mode_Tracking.py
@@ -4,17 +4,14 @@ import matplotlib.pyplot as mpl
 
 ### Paramètres du tuyau ###
 L = 0.5
-N = 4
-umax = 15
-du = 0.1
-mode1 = 0
-mode2 = 4
-mode3 = 2
-mode4 = 6
+N = 10
+umax = 20
+du = 0.01
+
 
 ### Paramtètres adimensionnés ###
-gamma = 0
-beta = 0.2
+gamma = 10
+beta = 0.65
 
 ### Fonction donnant les valeurs propres de poutre ###
 def Eq_Cara(Lambda):
@@ -83,7 +80,7 @@ MM = np.eye(N)
 
 
 def result(u):
-    C_g = 2*beta**0.5*u*B
+    C_g = 2*(beta**0.5)*u*B
     K = Delta + gamma*B + (u**2-gamma)*C + gamma*D
     F = np.block([[np.zeros((N,N)),MM],[MM,C_g]])
     E = np.block([[-MM,np.zeros((N,N))],[np.zeros((N,N)),K]])     
@@ -97,18 +94,14 @@ u_array = np.array([0])
 u = 0
 eigenValues, eigenVectors = result(u)
 print(eigenValues)
-# l = 0
-# while l < len(eigenValues):
-#         if eigenValues[l].imag < 0:
-#             eigenValues = np.delete(eigenValues,l)
-#             eigenVectors = np.concatenate((eigenVectors[:,:l], eigenVectors[:,l+1:]), axis = 1)
-#         else:    
-#             l += 1
 
 
-IM_Omega = np.array([(-1j*eigenValues).imag])
-RE_Omega = np.array([(-1j*eigenValues).real])
-Vectors = np.array([eigenVectors])
+arg = np.argsort(np.array([(-1j*eigenValues).real]))[0]
+
+IM_Omega = np.array([(-1j*eigenValues).imag[arg]])
+RE_Omega = np.array([(-1j*eigenValues).real[arg]])
+Vectors = np.array([eigenVectors[:,arg]])
+
 
 i = 0 
 while u < umax:
@@ -118,47 +111,37 @@ while u < umax:
     
     eigenValues, eigenVectors = result(u_tempo)
     
-    # l = 0
-    # while l < len(eigenValues):
-    #     if eigenValues[l].imag < 0:
-    #         eigenValues = np.delete(eigenValues,l)
-    #         eigenVectors = np.concatenate((eigenVectors[:,:l], eigenVectors[:,l+1:]), axis = 1)
-    #     else:    
-    #         l += 1
-    
 
     diff = np.zeros((2*N,2*N))
     for l in range(2*N):
         for k in range(2*N):
-            diff[l,k] = MAC(Vectors[i-1,l],eigenVectors[:,k])
+            diff[l,k] = MAC(Vectors[i-1,:,l],eigenVectors[:,k])
         
           
     
-    while min(np.max(diff,1)) < 0.8 and du_tempo > 10**-10:
+    while min(np.max(diff,1)) < 0.99999 and du_tempo > 10**-10:
         
         u_tempo = u+du_tempo/2
         du_tempo = du_tempo/2
         eigenValues, eigenVectors = result(u)
         for l in range(2*N):
             for k in range(2*N):
-                diff[l,k] = MAC(Vectors[i-1,l],eigenVectors[:,k])
-        #print(min(np.max(diff,1)))
-        #print(u_tempo)
-        
+                diff[l,k] = MAC(Vectors[i-1,:,l],eigenVectors[:,k])
+
     
     u = u_tempo
-    print(u)
-    print(min(np.max(diff,1)))
     u_array = np.append(u_array,u)
     arg_Max = np.argmax(diff,1) 
     IM_Omega = np.append(IM_Omega,[(-1j*eigenValues[arg_Max]).imag],axis=0)
     RE_Omega = np.append(RE_Omega,[(-1j*eigenValues[arg_Max]).real],axis=0)
     Vectors = np.append(Vectors,np.array([eigenVectors[:,arg_Max]]),axis=0)
-    # for k in range(N):
-    #     if MAC(Vectors[i-1,k],Vectors[i,k]) < 0.9:
-    #         print(IM_Omega[i,k]-IM_Omega[i-1,k])
-    #         print(RE_Omega[i,k]-RE_Omega[i-1,k])
-    #         print(MAC(Vectors[i-1,k],Vectors[i,k]))
+
+   
+   
+mode1 = N
+mode2 = N+1
+mode3 = N+2
+mode4 = N+3
    
 mpl.plot(RE_Omega[:,mode1],IM_Omega[:,mode1],label="Mode 1")
 mpl.plot(RE_Omega[:,mode2],IM_Omega[:,mode2],label="Mode 2")
